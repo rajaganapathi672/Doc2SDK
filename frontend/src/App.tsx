3ece5ced21fbf330ad22c4dcd36bb6eb8acef336
rajaganapathi672 <ganapathiraja672@gmail.com>
2026-01-26 09:40:00 +0530

    App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Workspace from './pages/Workspace';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <main className="main-content">
+        <Workspace />
+      </main>
+    </div>
+  );
+}
+
+export default App;
